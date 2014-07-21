a926eba01a4050ec9992173b67a4f197cc3666f7
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2014-07-21 18:20:47 +0200

    Automatic menu

--- Menu/Builder.php
+++ Menu/Builder.php
@@ -1,17 +1,21 @@
 <?php
 namespace Melody\AdminBundle\Menu;
 
+use Admingenerator\GeneratorBundle\Menu\AdmingeneratorMenuBuilder;
 use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-class Builder
+class Builder extends AdmingeneratorMenuBuilder
 {
 
-    private $factory;
+    protected $factory;
+    protected $container;
 
-    public function __construct(FactoryInterface $factory)
+    public function __construct(FactoryInterface $factory, ContainerInterface $container)
     {
         $this->factory = $factory;
+        $this->container = $container;
     }
 
     public function createAdminMenu(Request $request)
@@ -20,11 +24,32 @@ class Builder
         $router = $this->container->get('router');
         $collection = $router->getRouteCollection();
         $allRoutes = $collection->all();
-        foreach( $allRoutes as $route => $params ) {
-            if ( isset($params['type']) && $params['type'] == 'admingenerator' ) {
-                $menu->addChild('Module x', array(
-                    'route' => $route
-                ));
+        $menuElements = array();
+        foreach( $allRoutes as $routeName => $route ) {
+            /* @var $route \Symfony\Component\Routing\Route */
+            $defaults = $route->getDefaults();
+            if (
+                strpos( $route->getPath(), '/admin' ) === 0 &&
+                strpos( $routeName, '_list' ) > 0 &&
+                isset($defaults['_controller'])
+            ) {
+                $controllerAction = explode(':', $defaults['_controller']);
+                $controllerClass = $controllerAction[0];
+                $controller = new $controllerClass();
+                if (
+                    $controller instanceof \Admingenerator\GeneratorBundle\Controller\Doctrine\BaseController ||
+                    $controller instanceof \Admingenerator\GeneratorBundle\Controller\Propel\BaseController
+                ) {
+                    var_dump($controllerClass);
+                    $menuElements['Test'][$routeName] = $routeName;
+                }
+            }
+        }
+        $menu->setChildrenAttributes(array('id' => 'main_navigation', 'class' => 'nav navbar-nav'));
+        foreach( $menuElements as $menuGroup => $menuElementsPerGroup ) {
+            $group = $this->addDropdown($menu, $menuGroup);
+            foreach( $menuElementsPerGroup as $label => $route ) {
+                $this->addLinkRoute($group, $label, $route);
             }
         }
         return $menu;
