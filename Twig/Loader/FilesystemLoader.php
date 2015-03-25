516a8ec60d5ab91cc21118d5464c0297ad85d121
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2015-03-25 12:06:55 +0100

    Missing backslash

--- Twig/Loader/FilesystemLoader.php
+++ Twig/Loader/FilesystemLoader.php
@@ -2,7 +2,7 @@
 
 namespace Melody\AdminBundle\Twig\Loader;
 
-class FilesystemLoader extends Symfony\Bundle\TwigBundle\Loader\FilesystemLoader {
+class FilesystemLoader extends \Symfony\Bundle\TwigBundle\Loader\FilesystemLoader {
     
     protected function validateName($name)
     {
